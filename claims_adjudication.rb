db70ddbba1cdb3932beb7bc8479d8e59ab35f54e
Neil Rosenstech <neil.rosenstech@shopify.com>
2021-01-20 09:52:12 -0500

    Refactoring

--- claims_adjudication.rb
+++ claims_adjudication.rb
@@ -12,25 +12,9 @@ require 'date'
 
 class ClaimsAdjudication
   def adjudicate(contract, new_claim)
-    # TODO: Claims amount is not connected to purchase order amounts
-    claim_total = 0.0
-    contract.claims.each { |claim|
-      claim_total += claim.amount
-    }
-    # TODO: Refactor towards the Limit of Liability as an explicit concept
-    # Some kind of 80% factoring in here
-    if limit_of_liability_reached?(contract, claim_total, new_claim)
+    if contract.covers?(new_claim)
       contract.claims << new_claim
     end
-  end
-
-  # TODO: Update contract to tell us if it is active
-  def limit_of_liability_reached?(contract, claim_total, new_claim)
-    (contract.purchase_price - claim_total) * 0.8 > new_claim.amount &&
-      # Is within the contract period?
-      new_claim.date  >= contract.effective_date &&
-      new_claim.date  <= contract.expiration_date &&
-      contract.status == "ACTIVE"
   end
 
 # TODO: Check this with domain expert
--- contract.rb
+++ contract.rb
@@ -1,3 +1,5 @@
+# frozen_string_Literal: true
+
 require_relative './product'
 
 class Contract
@@ -13,10 +15,41 @@ class Contract
 
   attr_accessor :claims
 
+  ACTIVE_STATUS = 'ACTIVE'
+
   def initialize(purchase_price, covered_product)
     @purchase_price     = purchase_price
     @status             = "PENDING"
     @covered_product    = covered_product
     @claims             = Array.new
+  end
+
+  def covers?(new_claim)
+     within_liability?(new_claim) && in_effect_for?(new_claim)
+  end
+
+  private
+
+  def claim_total
+    claims.sum(&:amount)
+  end
+
+  def limit_of_liability
+    (purchase_price - claim_total) * 0.8
+  end
+
+  def within_liability?(claim)
+    limit_of_liability > claim.amount
+  end
+
+  # TODO: Ask domain expoert about naming
+  def in_effect_for?(claim)
+    active? &&
+      claim.date  >= effective_date &&
+      claim.date  <= expiration_date
+  end
+
+  def active?
+    status == ACTIVE_STATUS
   end
 end
