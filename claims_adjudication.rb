0b67c3a3edd647ff6745155213f6785ce5163c37
Paul Rayner <paul@virtualgenius.com>
2021-01-20 07:22:25 -0700

    Refactor out limit_of_liability_reached

--- claims_adjudication.rb
+++ claims_adjudication.rb
@@ -12,15 +12,29 @@ require 'date'
 
 class ClaimsAdjudication
   def adjudicate(contract, new_claim)
+    # TODO: Claims amount is not connected to purchase order amounts
     claim_total = 0.0
     contract.claims.each { |claim|
       claim_total += claim.amount
     }
-    if (contract.purchase_price - claim_total) * 0.8 > new_claim.amount &&
-      new_claim.date  >= contract.effective_date &&
-      new_claim.date  <= contract.expiration_date &&
-      contract.status == "ACTIVE"
+    # TODO: Refactor towards the Limit of Liability as an explicit concept
+    # Some kind of 80% factoring in here
+    if limit_of_liability_reached?(contract, claim_total, new_claim)
       contract.claims << new_claim
     end
   end
+
+  # TODO: Update contract to tell us if it is active
+  def limit_of_liability_reached?(contract, claim_total, new_claim)
+    (contract.purchase_price - claim_total) * 0.8 > new_claim.amount &&
+      # Is within the contract period?
+      new_claim.date  >= contract.effective_date &&
+      new_claim.date  <= contract.expiration_date &&
+      contract.status == "ACTIVE"
+  end
+
+# TODO: Check this with domain expert
+#  def limit_of_liability
+#    (contract.purchase_price - claim_total) * 0.8
+#  end
 end
