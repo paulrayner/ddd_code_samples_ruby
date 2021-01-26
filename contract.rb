5d3822c6c629626fcad6189ab3dde868fce3d72f
Paul Rayner <paul@virtualgenius.com>
2021-01-25 18:03:30 -0700

    Entity support for contract

--- contract.rb
+++ contract.rb
@@ -1,7 +1,9 @@
+require 'securerandom'
+
 require_relative './product'
 
 class Contract
-  attr_reader   :id # unique id (assigned automatically)
+  attr_reader   :id # unique id
   attr_reader   :purchase_price
   attr_reader   :covered_product
 
@@ -14,9 +16,15 @@ class Contract
   attr_accessor :claims
 
   def initialize(purchase_price, covered_product)
+    @id                 = SecureRandom.uuid
     @purchase_price     = purchase_price
     @status             = "PENDING"
     @covered_product    = covered_product
     @claims             = Array.new
+  end
+
+  # Equality for entities is based on unique id
+  def ==(other)
+    self.id == other.id
   end
 end
--- contract_test.rb
+++ contract_test.rb
@@ -11,4 +11,18 @@ class ContractTest < Test::Unit::TestCase
     assert_equal "PENDING", contract.status
     assert_equal Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0"), contract.covered_product
   end
+
+  # entities compare by unique IDs, not properties
+  def test_contract_equality
+    product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
+    contract = Contract.new(100.0, product)
+
+    contract_same_id         = contract.clone
+    contract_same_id.status  = "ACTIVE"
+
+    assert_equal     contract, contract_same_id
+
+    contract_different_id = Contract.new(100.0, product)
+    assert_not_equal contract, contract_different_id
+  end
 end
