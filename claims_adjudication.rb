ea1dbd14d876b53db9a48bb5294da891e67ade87
Paul Rayner <paul@virtualgenius.com>
2021-01-26 06:22:09 -0700

    Add documentation for adjudication

--- claims_adjudication.rb
+++ claims_adjudication.rb
@@ -2,6 +2,14 @@ require_relative './contract'
 require_relative './claim'
 require 'date'
 
+# Adjudicate/adjudication - a judgment made on a claim to determine whether
+# we are legally obligated to process the claim against the warranty. From
+# Wikipedia (https://en.wikipedia.org/wiki/Adjudication):
+#
+#  "Claims adjudication" is a phrase used in the insurance industry to refer to
+#  the process of paying claims submitted or denying them after comparing claims
+#  to the benefit or coverage requirements.
+
 class ClaimsAdjudication
   def adjudicate(contract, new_claim)
     if (new_claim.amount < contract.limit_of_liability &&
