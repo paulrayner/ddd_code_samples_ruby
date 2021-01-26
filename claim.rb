48d29d7ea3c2500694fc1a9428f693af6836d543
Paul Rayner <paul@virtualgenius.com>
2021-01-25 18:09:33 -0700

    Entity equality for Claim

--- claim.rb
+++ claim.rb
@@ -1,11 +1,19 @@
+require 'securerandom'
+
 class Claim
-  attr_reader   :id # unique id (assigned automatically)
+  attr_reader   :id # unique id
   attr_reader   :amount, :date
   attr_accessor :repair_pos
 
   def initialize(amount, date)
+    @id         = SecureRandom.uuid
     @amount     = amount
     @date       = date
     @repair_pos = Array.new
+  end
+
+  # Equality for entities is based on unique id
+  def ==(other)
+    self.id == other.id
   end
 end
--- claim_test.rb
+++ claim_test.rb
@@ -10,7 +10,7 @@ class ClaimTest < Test::Unit::TestCase
   def test_claim_is_setup_correctly
     line_item1  = LineItem.new("PARTS", 45.0, "Replacement part for soap dispenser")
     line_item2  = LineItem.new("LABOR", 50.0, "1 hour repair")
-    repair_po   = RepairPO.new()
+    repair_po   = RepairPO.new
     repair_po.line_items << line_item1
     repair_po.line_items << line_item2
     claim       = Claim.new(150.0, Date.new(2010, 5, 8))
@@ -22,5 +22,18 @@ class ClaimTest < Test::Unit::TestCase
     assert_equal "PARTS", claim.repair_pos[0].line_items[0].type
     assert_equal 45.0, claim.repair_pos[0].line_items[0].amount
     assert_equal "Replacement part for soap dispenser", claim.repair_pos[0].line_items[0].description
+  end
+
+  # entities compare by unique IDs, not properties
+  def test_claim_equality
+    claim = Claim.new(150.0, Date.new(2010, 5, 8))
+
+    claim_same_id = claim.clone
+    claim_same_id.repair_pos << RepairPO.new
+
+    assert_equal claim, claim_same_id
+
+    claim_different_id = Claim.new(150.0, Date.new(2010, 5, 8))
+    assert_not_equal claim, claim_different_id
   end
 end
