2e203dd1c21c7a6966ae7768f9a8db4052dbdb92
Paul Rayner <paul@virtualgenius.com>
2021-10-20 19:23:51 -0600

    Add comment to contract

--- contract.rb
+++ contract.rb
@@ -3,6 +3,11 @@ require_relative './terms_and_conditions'
 require_relative './customer_reimbursement_requested'
 require_relative './subscription_renewed'
 
+# Contract represents an extended warranty for a covered product.
+# A contract is in a PENDING state prior to the effective date,
+# ACTIVE between effective and expiration dates, and EXPIRED after
+# the expiration date.
+
 class Contract
   attr_reader   :id # unique id
 
