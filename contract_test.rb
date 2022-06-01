c87355500e8db6fc393ef8714ec41accfa5f8f99
Paul Rayner <paul@virtualgenius.com>
2022-06-01 07:50:13 -0600

    Spacing

--- contract_test.rb
+++ contract_test.rb
@@ -7,6 +7,7 @@ class ContractTest < Test::Unit::TestCase
   def test_contract_is_setup_correctly
     product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
     contract = Contract.new(100.0, product)
+    
     assert_equal 100, contract.purchase_price
     assert_equal "PENDING", contract.status
     assert_equal Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0"), contract.covered_product
