bde252c25c0a81ba93496bc54a037d6645698ce4
Paul Rayner <paul@virtualgenius.com>
2023-05-30 15:56:37 -0600

    Add dates to Contract constructor

--- contract_test.rb
+++ contract_test.rb
@@ -6,7 +6,7 @@ class ContractTest < Test::Unit::TestCase
 
   def test_contract_is_setup_correctly
     product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
-    contract = Contract.new(100.0, product)
+    contract = Contract.new(100.0, product, Date.new(2010, 5, 8), Date.new(2010, 5, 8), Date.new(2012, 5, 8))
 
     assert_equal 100, contract.purchase_price
     assert_equal "PENDING", contract.status
@@ -16,14 +16,14 @@ class ContractTest < Test::Unit::TestCase
   # entities compare by unique IDs, not properties
   def test_contract_equality
     product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
-    contract = Contract.new(100.0, product)
+    contract = Contract.new(100.0, product, Date.new(2010, 5, 8), Date.new(2010, 5, 8), Date.new(2012, 5, 8))
 
     contract_same_id         = contract.clone
     contract_same_id.status  = "ACTIVE"
 
     assert_equal     contract, contract_same_id
 
-    contract_different_id = Contract.new(100.0, product)
+    contract_different_id = Contract.new(100.0, product, Date.new(2010, 5, 8), Date.new(2010, 5, 8), Date.new(2012, 5, 8))
     assert_not_equal contract, contract_different_id
   end
 end
