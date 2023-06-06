4d90b1df723f4c63c6e582b946e0c0e0e6b21650
Paul Rayner <paul@virtualgenius.com>
2023-06-06 11:14:26 +0200

    Fix product test

--- product_test.rb
+++ product_test.rb
@@ -1,22 +1,24 @@
 require "test/unit"
-require "date"
+
 require_relative './product'
 
 class ProductTest < Test::Unit::TestCase
+
   def test_product_equality
-    product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0");
+    # A value object must be created whole
+    product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
 
-    # Demonstrate equality by property
+    # Demonstrate equality of object by property
     assert_equal Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0"), product
-end
+  end
 
-def test_product_inequality
-    product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0");
-    
-    # Demonstrate inequality by property
+  def test_product_inequality
+    product  = Product.new("dishwasher", "OEUOEU23", "Whirlpool", "7DP840CWDB0")
+
+    # Demonstrates inequality by property
     assert_not_equal Product.new("stove", "OEUOEU23", "Whirlpool", "7DP840CWDB0"), product
-    assert_not_equal Product.new("dishwasher", "BEUOEU23", "Whirlpool", "7DP840CWDB0"), product
+    assert_not_equal Product.new("dishwasher", "EUOEUOE", "Whirlpool", "7DP840CWDB0"), product
     assert_not_equal Product.new("dishwasher", "OEUOEU23", "Maytag", "7DP840CWDB0"), product
-    assert_not_equal Product.new("dishwasher", "OEUOEU23", "Whirlpool", "99999"), product
+    assert_not_equal Product.new("dishwasher", "OEUOEU23", "Whirlpool", "99999999"), product
   end
 end
